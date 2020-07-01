ec9a6311cefdc14fa3a3657df9f844db9ca170cb
Lennart Eichhorn <lennart@madmanfred.com>
2020-07-01 12:03:48 +0200

    Implemented Response class

--- src/response.cpp
+++ src/response.cpp
@@ -5,7 +5,7 @@ namespace jsonrpc{
 Response::Response(const QJsonObject& message): Message(message)
 {
     result = message.value("result");
-    if(message.value("result") == QJsonValue::Undefined){
+    if(result.isUndefined()){
         throw "Invalid result";
     }
 }
@@ -17,17 +17,25 @@ QJsonValue Response::getResult() const
 
 QJsonObject Response::toJson() const
 {
-
+    QJsonObject resultMessageJson = Message::toJson();
+    resultMessageJson.insert("result",result);
+    return resultMessageJson;
 }
 
-Response::Response(const QJsonValue &result)
+Response::Response(const QJsonValue &result):
+    Message(), result(result)
 {
-
+    if(this->result.isUndefined()){
+        this->result = QJsonValue(QJsonValue::Null);
+    }
 }
 
-Response::Response(const QJsonValue &id, const QJsonValue &result)
+Response::Response(const QJsonValue &id, const QJsonValue &result):
+    Message(id), result(result)
 {
-
+    if(this->result.isUndefined()){
+        this->result = QJsonValue(QJsonValue::Null);
+    }
 }
 
 }
